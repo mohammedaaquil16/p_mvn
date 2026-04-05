34434c7e7b187577610df41231df705af57f2894
mohammedaaquil16 <mohammed.aaquil2023@vitstudent.ac.in>
2026-04-05 22:17:25 +0530

    add

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,13 @@
 package com.example;
 
 public class App {
+
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins CI/CD!");
+    }
+
+    // Simple method for testing
+    public int add(int a, int b) {
+        return a + b;
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,10 +1,14 @@
 package com.example;
 
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
 
     @Test
+    public void testAdd() {
         App app = new App();
+        int result = app.add(2, 3);
+        assertEquals(5, result);
+    }
 }
